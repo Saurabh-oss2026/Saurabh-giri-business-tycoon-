c088221a72635e77e5fd0249d088d1c8555b8c45
Saurabh-oss2026 <girisourabh75@gmail.com>
2026-07-04 08:58:40 +0530

    Update Customer.js

--- Customer.js
+++ Customer.js
@@ -1,1 +1,22 @@
+// customer.js
 
+const customerNames = [
+    "Rahul",
+    "Aman",
+    "Priya",
+    "Riya",
+    "Vikas",
+    "Neha"
+];
+
+function randomCustomer() {
+    const name =
+        customerNames[Math.floor(Math.random() * customerNames.length)];
+
+    document.getElementById("customer").innerHTML =
+        "👤 " + name + " bought tea!";
+
+    earn();
+}
+
+setInterval(randomCustomer, 5000);
